da2804b27159cfe5e4831f504a1aa667511b8443
Nikita.Dovnich <n.dovnich@gmail.com>
2019-11-14 23:10:03 +0200

    +++

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+
+
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TimetableComponent } from './timetable/timetable.component';
+import { ContactComponent } from './contact/contact.component';
+import { ChatComponent } from './chat/chat.component';
+
+
+const routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'about', component: AboutComponent },
+{ path: 'timeTable', component: TimetableComponent },
+{ path: 'contact', component: ContactComponent },
+{ path: 'chat', component: ChatComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
